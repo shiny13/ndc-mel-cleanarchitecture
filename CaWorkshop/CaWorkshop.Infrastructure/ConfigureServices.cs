439f537d90a2c55e2caec54b5405418a5aac102d
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-07-20 23:23:34 +1000

    Added the Infrastructure project

---
+++ CaWorkshop/CaWorkshop.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,42 @@
+﻿using CaWorkshop.Infrastructure.Identity;
+using CaWorkshop.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Infrastructure.Data.Interceptors;
+
+namespace CaWorkshop.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IApplicationDbContext>(sp => 
+            sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddScoped<ApplicationDbContextInitialiser>();
+
+        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+
+        services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        services.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        services.AddScoped<IIdentityService, IdentityService>();
+
+        return services;
+    }
+}
---
+++ CaWorkshop/CaWorkshop.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+using CaWorkshop.Application.Common.Interfaces;
+using CaWorkshop.Domain.Entities;
+using CaWorkshop.Infrastructure.Data.Interceptors;
+using CaWorkshop.Infrastructure.Identity;
+
+using Duende.IdentityServer.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CaWorkshop.Infrastructure.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
+{
+    private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+    public ApplicationDbContext(
+        DbContextOptions options, 
+        IOptions<OperationalStoreOptions> operationalStoreOptions,
+        AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor)
+        : base(options, operationalStoreOptions)
+    {
+        _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+    }
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+#if DEBUG
+        optionsBuilder
+            .EnableDetailedErrors();
+#endif
+
+        optionsBuilder
+            .AddInterceptors(_auditableEntitySaveChangesInterceptor);
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {   
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
