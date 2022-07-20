b8b81ba02b56cc1d65b10a73923581c1496b1bcb
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-07-21 00:46:30 +1000

    added controllers and filter in web.ui

---
+++ CaWorkshop/CaWorkshop.WebUI/Controllers/TodoItemsController.cs
@@ -0,0 +1,56 @@
+using CaWorkshop.Application.TodoItems.Commands.CreateTodoItem;
+using CaWorkshop.Application.TodoItems.Commands.DeleteTodoItem;
+using CaWorkshop.Application.TodoItems.Commands.UpdateTodoItem;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace CaWorkshop.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodoItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // POST: api/TodoItems
+        [HttpPost]
+        public async Task<ActionResult<long>> PostTodoItem(
+            CreateTodoItemCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        // PUT: api/TodoItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id,
+            UpdateTodoItemCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            await _mediator.Send(new DeleteTodoItemCommand { Id = id });
+
+            return NoContent();
+        }
+    }
+}
---
+++ CaWorkshop/CaWorkshop.WebUI/Controllers/TodoListsController.cs
@@ -0,0 +1,60 @@
+using CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
+using CaWorkshop.Application.TodoLists.Commands.DeleteTodoList;
+using CaWorkshop.Application.TodoLists.Commands.UpdateTodoList;
+using CaWorkshop.Application.TodoLists.Queries.GetTodoLists;
+using CaWorkshop.Domain.Entities;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaWorkshop.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TodoListsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/TodoLists
+        [HttpGet]
+        public async Task<ActionResult<TodosVm>> GetTodoLists()
+        {
+            return await _mediator.Send(new GetTodoListsQuery());
+        }
+
+        // POST: api/TodoLists
+        [HttpPost]
+        public async Task<ActionResult<int>> PostTodoList(
+            CreateTodoListCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        // PUT: api/TodoLists/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoList(int id,
+            UpdateTodoListCommand command)
+        {
+            if (id != command.Id) return BadRequest();
+
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodoLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoList(int id)
+        {
+            await _mediator.Send(new DeleteTodoListCommand { Id = id });
+
+            return NoContent();
+        }
+    }
+}
