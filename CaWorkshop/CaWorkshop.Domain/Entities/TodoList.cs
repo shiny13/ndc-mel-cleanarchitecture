5cf3b0c0e6de25e67fe99b04cf1d59f4ce8f7847
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-07-20 23:21:04 +1000

    Added the Domain project

---
+++ CaWorkshop/CaWorkshop.Domain/Entities/TodoList.cs
@@ -0,0 +1,16 @@
+﻿
+using CaWorkshop.Domain.Common;
+
+namespace CaWorkshop.Domain.Entities;
+
+public class TodoList : AuditableEntity
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Colour { get; set; }
+
+    public IList<TodoItem> Items { get; set; }
+        = new List<TodoItem>();
+}
