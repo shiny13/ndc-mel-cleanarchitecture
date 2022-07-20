5706da6310ad8b3b5958fe33b243dc9901a724f3
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2022-07-20 23:18:55 +1000

    Added the application project

---
+++ CaWorkshop/CaWorkshop.Application/ConfigureServices.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Reflection;
+
+using CaWorkshop.Application.Common.Behaviours;
+
+using FluentValidation;
+
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CaWorkshop.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),
+            typeof(PerformanceBehaviour<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),
+            typeof(UnhandledExceptionBehaviour<,>));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehaviour<,>));
+
+        return services;
+    }
+}
